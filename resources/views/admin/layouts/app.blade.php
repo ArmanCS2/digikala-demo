0e3ce163a65cb1a3739d3c7bb556341ae8a59d67
ArmanCS <armanafzali31@gmail.com>
2023-08-22 15:12:07 +0330

    create layouts

---
+++ resources/views/admin/layouts/app.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    @include('admin.layouts.head-tag')
+    @yield('head-tag')
+</head>
+
+<body dir="rtl">
+
+
+@include('admin.layouts.header')
+
+<section class="body-container">
+    @include('admin.layouts.sidebar')
+    <section id="main-body" class="main-body">
+        @yield('content')
+    </section>
+</section>
+
+
+@include('admin.layouts.scripts')
+@yield('scripts')
+</body>
+
+</html>
---
+++ resources/views/admin/layouts/scripts.blade.php
@@ -0,0 +1,4 @@
+<script src="assets/js/jquery-3.5.1.min.js"></script>
+<script src="assets/js/popper.js"></script>
+<script src="assets/js/bootstrap/bootstrap.min.js"></script>
+<script src="assets/js/grid.js"></script>
