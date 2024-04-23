a02009b06091445c1ee93448635ea9d41fdf5c66
ArmanCS <armanafzali31@gmail.com>
2024-04-23 18:22:55 +0330

    save changes

--- resources/views/app/layouts/head-tag.blade.php
+++ resources/views/app/layouts/head-tag.blade.php
@@ -1,9 +1,9 @@
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-<meta name="author" content="butikala">
+<meta name="author" content="بوتیکالا">
 <link rel="icon" href="{{asset(\App\Models\Setting\Setting::first()->icon)}}">
 <link rel="shortcut icon" href="{{asset(\App\Models\Setting\Setting::first()->icon)}}">
-<link rel="alternate" href="https://butikala.ir" hreflang="fa-ir">
+<link rel="alternate" href="https://www.butikala.ir" hreflang="fa-ir">
 <link rel="canonical" href="{{url()->current()}}">
 <!-- Bootstrap CSS -->
 <link rel="stylesheet" href="{{asset('app-assets/css/bootstrap/bootstrap-reboot.rtl.min.css')}}">
